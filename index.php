ba3a0d6fd1b98351bd0b888f091a3e6b9ba23657
Nidhi <nidhipoojary1211@gmail.com>
2023-10-11 00:55:08 +0530

    images logo.png index.css and index.php

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 	<head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="index.css">
 		<title>Document</title>
 	</head>
 	<body>
+		<div class="bg">Discover.<br>Trek.<br>Inspire.<br></div>
+
+
+
 		
 	<?php
 		if(isset($_FILES['fileToUpload'])){

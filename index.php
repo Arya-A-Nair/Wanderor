40a07794e594243424c1a5eedbd529397a8de966
Nidhi <nidhipoojary1211@gmail.com>
2023-10-11 01:18:54 +0530

    images logo.png index.css and index.php

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 	<head>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link href='https://fonts.googleapis.com/css?family=Lexend' rel='stylesheet'>
 		<link rel="stylesheet" href="index.css">
 		<title>Document</title>
 	</head>
 	<body>
-	<div class="container">
-        <img class="image" src="images/backgroundimg.png" alt="Your Image">
-        <div class="text">Discover.<br>Trek.<br>Inspire.<br></div>
-    </div>
+		<div class="container">
+			<img class="image" src="images/backgroundimg.png" alt="Your Image">
+			<div class="text">Discover.<br>Trek.<br>Inspire.<br></div>
+		</div>
 
 
 

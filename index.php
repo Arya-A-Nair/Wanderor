5b2c9002b1fca421e75ddee19f0d92a2b40e1463
Nidhi <nidhipoojary1211@gmail.com>
2023-10-22 01:26:22 +0530

    check navbar

--- index.php
+++ index.php
@@ -4,24 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@600&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Leckerli+One" rel="stylesheet">
     <title>Document</title>
     <style>
-        /* Reset default margin and padding for the body */
+        
         body {
             margin: 0;
             padding: 0;
         }
 
-        /* Style for the navbar */
+        
         .navbar {
             background-color: rgba(0, 0, 0, 0.7);
             display: flex;
-            justify-content: space-between; /* Push elements to the edges */
+            justify-content: space-between; 
             align-items: center;
             padding: 20px;
         }
 
-        /* Style for the navbar elements */
+       
         .navbar a {
             text-decoration: none;
             font-family: 'Lexend Deca';
@@ -30,7 +31,7 @@
             margin: 0 10px;
         }
 
-        /* Style for the logout button */
+        
         .logout-btn {
             background-color: white;
             color: black;
@@ -82,7 +83,7 @@
             display: flex;
         }
 
-		.written{
+		.written {
 			color: #000;
 			font-family: 'Lexend Deca';
 			font-size: 1.3rem;
@@ -97,25 +98,64 @@
 			flex-shrink: 0;
 		}
 
-		.cimg-container {
-			display: flex;
-			flex-direction: row;
-			align-items: center;
-			overflow-x: auto; /* Allow horizontal scrolling if the content exceeds the screen width */
-		}
+    .cimg-container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        overflow-x: auto; /* Allow horizontal scrolling if the content exceeds the screen width */
+    }
 
-		.cimg {
-			width: 15rem;
-			height: 12rem;
-			border-radius: 5rem;
-			margin: 1.5rem;
-		}
+    .cimg {
+        width: 15rem;
+        height: 12rem;
+        border-radius: 5rem;
+        margin: 1.5rem;
+        position: relative;
+        transition: transform 0.3s; /* Add a transition for smooth scaling */
+    }
+
+    .cimg:hover {
+        transform: scale(1.1); /* Scale the image up on hover */
+        z-index: 1; /* Ensure the image is on top when scaled */
+    }
+
+	.smalltext{
+		color: #000;
+		text-align: center;
+		font-family: Leckerli One;
+		font-size: 1.5rem;
+		font-style: normal;
+		font-weight: 400;
+		line-height: 143%;
+	}
+
+	.centered-container {
+		margin: 5rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.text2{
+		color: #000;
+		text-align: center;
+		font-family: Lexend Deca;
+		font-size: 2.5rem;
+		font-style: normal;
+		font-weight: 600;
+		line-height: 118%; 
+		width: 30rem;
+	}
+
+
+
     </style>
 </head>
 <body>
     <div class="navbar">
-        <!-- Logo with a link to the home page -->
-        <a class="logo" href="./">
+        
+        <a class "logo" href="./">
             <img src="uploads/logo.png" alt="Your Logo" width="150" height="50">
         </a>
         <div class="right-section">
@@ -131,15 +171,15 @@
             }
             ?>
             <form method="POST">
-                <?php 
-                    include "utils/logoutindex.php";
-                    if (isset($_POST['logout'])) {
-                        logout();
-                    }
-                ?>
-                <input hidden="true" name "logout" value="true" />
-                <button type="submit" class="logout-btn">Logout</button>
-            </form>
+				<?php 
+					include "utils/logoutindex.php";
+					if(isset($_POST['logout'])){
+						logout();
+					}
+				?>
+				<input hidden="true" name="logout" value="true"/>
+				<button type="submit" class="logout-btn">Logout</button>
+				</form>
         </div>
     </div>
 
@@ -149,13 +189,26 @@
         <div class="belowtext">"Explore, Connect, and Trek the World's Wonders"</div>
     </div>
 
-    
-    
     <div class="cimg-container">
 	    <div class="written">Top destinations suggested by expert guides</div>
         <img class="cimg" src="uploads/image5.png" alt="Image 1">
         <img class="cimg" src="uploads/image6.png" alt="Image 2">
         <img class="cimg" src="uploads/image7.png" alt="Image 3">
     </div>
+	
+	<div class="centered-container">
+	<div class="text2">Start Exploring</div>
+	<div class="explore-buttons">
+        <button class="explore-button">Explore 1</button>
+        <button class="explore-button">Explore 2</button>
+        <button class="explore-button">Explore 3</button>
+    </div>
+	</div>
+
+	<div class="centered-container">
+		<img src="uploads/image9.png">
+		<div class="smalltext">Best Trek Operations</div><br><br>
+		<div class="text2">Excellence in the Trekking Industry</div>
+	</div>
 </body>
 </html>
